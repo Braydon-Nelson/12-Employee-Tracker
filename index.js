6c95508e05ac7536e83d536f58dc9675b9054670
Braydon Nelson <braydon.nelson@outlook.com>
2020-02-17 21:37:43 -0700

    added file structure

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(`Hello There`);
+const mysql = require('mysql');
+const db = require('./db/');
+
+const {
+    addEmployee,
+    deleteEmployee,
+    updateEmployeeRole,
+    updateEmployeeManager,
+    displayAllEmployees,
+    displayAllByManager,
+    displayAllByDepartment
+} = require('./controllers/employee');
+
+const {
+    addDepartment,
+    deleteDepartment,
+    displayAllDepartments
+} = require('./controllers/departments');
+
+const {
+    addRole,
+    deleteRole,
+    displayAllRoles
+} = require('./controllers/roles');
+
